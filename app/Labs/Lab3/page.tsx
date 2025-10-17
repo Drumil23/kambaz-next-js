ddad16d435d4b96fbfcd1d4327d3ab04a93d2548
Drumil23 <drumil.kotecha@gmail.com>
2025-10-17 14:59:59 -0400

    changed from var -> const due to errors

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -1,6 +1,7 @@
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
 export default function Lab3() {
   return (
     <div id="wd-lab3">
@@ -8,5 +9,6 @@ export default function Lab3() {
       <VariablesAndConstants />
       <VariableTypes />
       <BooleanVariables />
+      <IfElse />
     </div>
 );}
