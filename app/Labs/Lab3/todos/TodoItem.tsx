1f9dd127d4827ca453bf4a96136768f7f31e7d9c
Drumil23 <drumil.kotecha@gmail.com>
2025-12-01 19:41:50 -0500

    cleaned up TodoItem and TodoList imports for better readability

--- app/Labs/Lab3/todos/TodoItem.tsx
+++ app/Labs/Lab3/todos/TodoItem.tsx
@@ -1,16 +1,19 @@
 import { ListGroupItem } from 'react-bootstrap';
 export interface Todo {
-    done: boolean;
-    title: string;
-    status: 'COMPLETED' | 'PENDING' | 'IN_PROGRESS' | 'DEFERRED' | 'CANCELED';
+  done: boolean;
+  title: string;
+  status: 'COMPLETED' | 'PENDING' | 'IN_PROGRESS' | 'DEFERRED' | 'CANCELED';
 }
-const TodoItem = ( { todo = { done: true, title: 'Buy milk',
-                              status: 'COMPLETED' as const } }: { todo?: Todo }) => {
- return (
-   <ListGroupItem>
-     <input type="checkbox" className="me-2"
-            defaultChecked={todo.done}/>
-     {todo.title} ({todo.status})
-   </ListGroupItem>
- );}
+const TodoItem = ({ todo = {
+  done: true, title: 'Buy milk',
+  status: 'COMPLETED' as const
+} }: { todo?: Todo }) => {
+  return (
+    <ListGroupItem>
+      <input type="checkbox" className="me-2"
+        defaultChecked={todo.done} />
+      {todo.title} ({todo.status})
+    </ListGroupItem>
+  );
+}
 export default TodoItem;
--- app/Labs/Lab3/todos/TodoList.tsx
+++ app/Labs/Lab3/todos/TodoList.tsx
@@ -1,6 +1,9 @@
-import TodoItem from "./TodoItem";
-import todos from "./todos.json";
+import TodoItem, { Todo } from "./TodoItem";
+import todosData from "./todos.json";
 import { ListGroup } from 'react-bootstrap';
+
+const todos = todosData as Todo[];
+
 export default function TodoList() {
     return (
         <>
